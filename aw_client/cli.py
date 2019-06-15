b916d617de470149a21945474476e80fd599d827
Jacob Steele <jsteele@jsteeleir.com>
2019-06-14 23:09:59 -0700

    Add 'buckets' subparser to aw-cli, and 'add', 'delete' methods.

--- aw_client/cli.py
+++ aw_client/cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-import json
 import argparse
+import json
+import sys
 from datetime import timedelta, datetime, timezone
 from pprint import pprint
 
@@ -16,6 +17,38 @@ def _valid_date(s):
         msg = "Not a valid date: '{0}'.".format(s)
         raise argparse.ArgumentTypeError(msg)
 
+# From http://code.activestate.com/recipes/577058/
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
 
 def main():
     now = datetime.now(timezone.utc)
@@ -29,22 +62,38 @@ def main():
     subparsers = parser.add_subparsers(help='sub-command help')
 
     parser_heartbeat = subparsers.add_parser('heartbeat', help='Send a heartbeat to the server')
+    parser_buckets = subparsers.add_parser('buckets', help='List/Manage Buckets')
+    parser_events = subparsers.add_parser('events', help='List/Manage Events')
+    parser_query = subparsers.add_parser('query', help='Query events from bucket')
+
     parser_heartbeat.set_defaults(which='heartbeat')
     parser_heartbeat.add_argument('--pulsetime', default=60, help='Pulsetime to use')
     parser_heartbeat.add_argument('bucket', help='bucketname to send heartbeat to')
     parser_heartbeat.add_argument('data', default="{}", help='JSON data to send in heartbeat')
 
-    parser_buckets = subparsers.add_parser('buckets',
-                                           help='List all buckets')
     parser_buckets.set_defaults(which='buckets')
+    buckets_subparser = parser_buckets.add_subparsers(title='buckets', help="List/Manage Buckets")
+    buckets_list_parser = buckets_subparser.add_parser('list', help="List Buckets")
+    buckets_add_parser = buckets_subparser.add_parser('add', help="Add a Bucket")
+    buckets_delete_parser = buckets_subparser.add_parser('delete', help="Delete a Bucket")
 
-    parser_buckets = subparsers.add_parser('events',
-                                           help='Query events from bucket')
-    parser_buckets.set_defaults(which='events')
-    parser_buckets.add_argument('bucket')
+    buckets_list_parser.set_defaults(which='buckets.list')
+    buckets_add_parser.set_defaults(which='buckets.add')
+    buckets_add_parser.add_argument('bucket')
+    buckets_add_parser.add_argument('type')
+    buckets_delete_parser.set_defaults(which='buckets.delete')
+    buckets_delete_parser.add_argument('bucket')
+    buckets_delete_parser.add_argument('--force')
 
-    parser_query = subparsers.add_parser('query',
-                                         help='Query events from bucket')
+    parser_events.set_defaults(which='events')
+    parser_events.add_argument('bucket')
+    events_subparser = parser_events.add_subparsers(title='events', help="List/Manage Events")
+    events_list_parser = events_subparser.add_parser('list', help='List Events')
+    events_add_parser = events_subparser.add_parser('add', help='Add Events')
+    #  TODO:  <09-06-19, jsteeleir> # Add Modify Events
+    #events_mod_parser = events_subparser.add_parser('modify', help='Modify Events')
+    events_delete_parser = events_subparser.add_parser('delete', help='Delete Events')
+
     parser_query.set_defaults(which='query')
     parser_query.add_argument('path')
     parser_query.add_argument('--name')
@@ -62,11 +111,18 @@ def main():
         e = Event(duration=0, data=json.loads(args.data), timestamp=now)
         print(e)
         client.heartbeat(args.bucket, e, args.pulsetime)
-    elif args.which == "buckets":
+    elif args.which == "buckets.list":
         buckets = client.get_buckets()
         print("Buckets:")
         for bucket in buckets:
             print(" - {}".format(bucket))
+    elif args.which == "buckets.add":
+        client.create_bucket(args.bucket, args.type)
+    elif args.which == "buckets.delete":
+        if args.force:
+            client.delete_bucket(args.bucket)
+        elif query_yes_no("Really Delete bucket %s" % args.bucket, "no"):
+            client.delete_bucket(args.bucket)
     elif args.which == "events":
         events = client.get_events(args.bucket)
         print("events:")
